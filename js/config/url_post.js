7dd2201463fbf83890ff11f8c155a99a9ead388d
Nidasakinaa <nidasakinaaulia@gmail.com>
2024-05-31 15:55:40 +0700

    push

---
+++ js/config/url_post.js
@@ -0,0 +1,7 @@
+export let urlPOST = " https://ws-nida2024-72645f997179.herokuapp.com/insert"
+
+export function AmbilResponse(result) {
+    console.log(result); //menampilkan response API pada console
+    alert(result.message); //menampilkan response API pada alert
+    window.location.reload(); //reload halaman setelah klik ok pada alert
+}
